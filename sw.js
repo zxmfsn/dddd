3d614e2f4fdce5074a956d1c3ee3240ddd59c0ac
zxmfsn <cyxfvmwues783460@163.com>
2026-01-30 22:35:27 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,9 @@
-const CACHE_NAME = "cat-app-v1";
-const ASSETS = ["./", "./index.html", "./style.css", "./script.js", "./extra.js", "./cat.js"];
-
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
 });
+
+self.addEventListener("fetch", () => {});
