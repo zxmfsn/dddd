37bf0ed7a8661578df4b94fc6f8a0224cb6d42f3
zxmfsn <cyxfvmwues783460@163.com>
2026-01-30 22:47:57 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener("install", (event) => {
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener("fetch", () => {});
